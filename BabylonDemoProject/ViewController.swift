6a2f488670586f05bf25d71e3d5525e5c35f307b
Igor Kokoev <igrrik@gmail.com>
2020-06-11 00:20:35 +0300

    Add xcode project

---
+++ BabylonDemoProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BabylonDemoProject
+//
+//  Created by Igor Kokoev on 11.06.2020.
+//  Copyright © 2020 Igor Kokoev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
