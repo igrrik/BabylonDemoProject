642f5cbc6523a5d41e20fd1e4990ed168b7e043e
Igor Kokoev <igrrik@gmail.com>
2020-06-11 01:12:10 +0300

    Add swiftlint

--- BabylonDemoProject/SceneDelegate.swift
+++ BabylonDemoProject/SceneDelegate.swift
@@ -5,19 +5,24 @@
 //  Created by Igor Kokoev on 11.06.2020.
 //  Copyright © 2020 Igor Kokoev. All rights reserved.
 //
+//  swiftlint:disable all
 
 import UIKit
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
-
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let _ = (scene as? UIWindowScene) else {
+            return
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -46,11 +51,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
-
-        // Save changes in the application's managed object context when the application transitions to the background.
-        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
-
-
 }
-
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # BabylonDemoProject
 Demo Project for Babylon Health
+
+# Setup
+
+1. Run `brew install swiftlint`
